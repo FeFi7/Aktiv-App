fd85bd8be742fd9d1514cb529af4149723378234
Fabian Bösel <fabian.boesel@web.de>
2021-04-07 02:31:39 +0200

    Code der Routen gekürzt und aufgeräumt

--- server/nodeJS/routes/veranstaltungRoute.js
+++ server/nodeJS/routes/veranstaltungRoute.js
@@ -1,85 +1,86 @@
-var veranstaltungService = require("../services/veranstaltungService")
-var express = require('express');
+var veranstaltungService = require("../services/veranstaltungService");
+var express = require("express");
 var router = express.Router();
 
 router.use(function timeLog(req, res, next) {
-  console.log( req.headers.host + ' Time: ', new Date().toISOString());
+  console.log(req.headers.host + " Time: ", new Date().toISOString());
   next();
 });
 
 // [GET] bekomme einzelne Veranstaltung
-router.get('/:veranstaltungId', async function(req, res) {
+router.get("/:veranstaltungId", async function (req, res) {
   const veranstaltungId = req.params.veranstaltungId;
   // ist query eine Zahl?
-  if(! /^\d+$/.test(veranstaltungId)){
-    res.status(400);
-    res.send("Id keine Zahl")
+  if (!/^\d+$/.test(veranstaltungId)) {
+    res.status(400).send("Id keine Zahl");
   }
-  
-  const veranstaltung = await veranstaltungService.getVeranstaltungById(req.params.veranstaltungId)
-  if(veranstaltung){
-    res.json(await veranstaltungService.getVeranstaltungById(req.params.veranstaltungId));
+
+  const veranstaltung = await veranstaltungService.getVeranstaltungById(
+    req.params.veranstaltungId
+  );
+  if (veranstaltung) {
+    res.json(
+      await veranstaltungService.getVeranstaltungById(
+        req.params.veranstaltungId
+      )
+    );
+  } else {
+    res.status(404).send("Veranstaltung nicht vorhanden");
   }
-  else{
-    res.status(404);
-    res.send("Veranstaltung nicht vorhanden")
-  }
-  
 });
 
 // [PUT] update einzelne Veranstaltung
-router.put('/:veranstaltungId', async function(req, res) {
+router.put("/:veranstaltungId", async function (req, res) {
   const veranstaltungId = req.params.veranstaltungId;
   // ist query eine Zahl?
-  if(! /^\d+$/.test(veranstaltungId)){
-    res.status(400);
-    res.send("Id keine Zahl")
+  if (!/^\d+$/.test(veranstaltungId)) {
+    res.status(400).send("Id keine Zahl");
   }
 
-  if(veranstaltungService.getVeranstaltungById(veranstaltungId) === null){
-    res.status(404);
-    res.send("Veranstaltung nicht vorhanden")
+  if (veranstaltungService.getVeranstaltungById(veranstaltungId) === null) {
+    res.status(404).send("Veranstaltung nicht vorhanden");
   }
-  
+
   res.json(veranstaltungService.getVeranstaltungById(veranstaltungId));
 });
 
 // [DELETE] lösche einzelne Veranstaltung
-router.delete('/:veranstaltungId', async function(req, res) {
-
-  res.json(veranstaltungService.getVeranstaltungById(req.params.veranstaltungId));
+router.delete("/:veranstaltungId", async function (req, res) {
+  res.json(
+    veranstaltungService.getVeranstaltungById(req.params.veranstaltungId)
+  );
 });
 
 // [GET] bekomme alle Veranstaltung
-router.get('/*', async function(req, res) {
-  const limit = 25
-  const page = 1
+router.get("/*", async function (req, res) {
+  const limit = 25;
+  const page = 1;
 
-  if(typeof req.query.limit !== 'undefined'){
+  if (req.query.limit) {
     // ist query eine Zahl?
-    if(/^\d+$/.test(req.query.limit) && req.query.limit < 100){
-        limit = req.query.limit
+    if (/^\d+$/.test(req.query.limit) && req.query.limit < 100) {
+      limit = req.query.limit;
     }
   }
-  if(typeof req.query.page !== 'undefined'){
-    if(/^\d+$/.test(req.query.page)){
-      page = req.query.page
+  if (req.query.page) {
+    if (/^\d+$/.test(req.query.page)) {
+      page = req.query.page;
     }
   }
 
-  const veranstaltungen = await veranstaltungService.getVeranstaltungen(limit) 
-  
-  res.send(veranstaltungen)
+  const veranstaltungen = await veranstaltungService.getVeranstaltungen(limit);
+
+  res.send(veranstaltungen);
 });
 
 // [POST] Erstelle eine Veranstaltung
-router.post('/*', async function(req, res) {
+router.post("/*", async function (req, res) {
   let titel = req.body.titel;
   let beschreibung = req.body.beschreibung;
   let kontakt = req.body.kontakt;
   let beginn_ts = req.body.beginn_ts;
   let ende_ts = req.body.ende_ts;
-  let ortBeschreibung = req.body.ortBeschreibung; 
+  let ortBeschreibung = req.body.ortBeschreibung;
   let latitude = req.body.latitude;
   let longitude = req.body.longitude;
   let institutionId = req.body.institutionId;
@@ -87,106 +88,96 @@ router.post('/*', async function(req, res) {
   let istGenehmigt = req.body.istGenehmigt;
 
   //-------------------------Überprüfung Parameter---------------------------
-  if(typeof titel === 'undefined'){
-    res.status(400);
-    res.send("titel benötigt");
+  if (!titel) {
+    res.status(400).send("titel benötigt");
     return;
   }
-  if(typeof beschreibung === 'undefined'){
-    res.status(400);
-    res.send("beschreibung benötigt");
+  if (!beschreibung) {
+    res.status(400).send("beschreibung benötigt");
     return;
   }
-  if(typeof kontakt === 'undefined'){
-    res.status(400);
-    res.send("kontakt benötigt");
+  if (!kontakt) {
+    res.status(400).send("kontakt benötigt");
     return;
   }
-  if(typeof beginn_ts === 'undefined'){
-    res.status(400);
-    res.send("beginn_ts benötigt");
+  if (!beginn_ts) {
+    res.status(400).send("beginn_ts benötigt");
     return;
   }
-  if(typeof ende_ts === 'undefined'){
-    res.status(400);
-    res.send("ende_ts benötigt");
+  if (!ende_ts) {
+    res.status(400).send("ende_ts benötigt");
     return;
   }
-  if(typeof ortBeschreibung === 'undefined'){
-    res.status(400);
-    res.send("ortBeschreibung benötigt");
+  if (!ortBeschreibung) {
+    res.status(400).send("ortBeschreibung benötigt");
     return;
   }
-  if(typeof latitude !== 'undefined'){
+  if (latitude) {
     // ist numerisch?
-    if(!/^-?\d+\.?\d*$/.test(latitude)){
-      res.status(400);
-      res.send("latitude muss numerisch sein");
+    if (!/^-?\d+\.?\d*$/.test(latitude)) {
+      res.status(400).send("latitude muss numerisch sein");
       return;
     }
-  }
-  else{
-    res.status(400);
-    res.send("latitude benötigt");
+  } else {
+    res.status(400).send("latitude benötigt");
     return;
   }
-  if(typeof longitude !== 'undefined'){
+  if (longitude) {
     // ist numerisch?
-    if(!/^-?\d+\.?\d*$/.test(longitude)){
-      res.status(400);
-      res.send("longitude muss numerisch sein");
+    if (!/^-?\d+\.?\d*$/.test(longitude)) {
+      res.status(400).send("longitude muss numerisch sein");
       return;
     }
-  }
-  else{
-    res.status(400);
-    res.send("longitude benötigt");
+  } else {
+    res.status(400).send("longitude benötigt");
     return;
   }
-  if(typeof institutionId !== 'undefined'){
+  if (institutionId) {
     // ist numerisch?
-    if(!/^-?\d+\.?\d*$/.test(institutionId)){
-      res.status(400);
-      res.send("institutionId muss numerisch sein");
+    if (!/^-?\d+\.?\d*$/.test(institutionId)) {
+      res.status(400).send("institutionId muss numerisch sein");
       return;
     }
-  }
-  else{
-    res.status(400);
-    res.send("institutionId benötigt");
+  } else {
+    res.status(400).send("institutionId benötigt");
     return;
   }
-  if(typeof userId !== 'undefined'){
+  if (userId) {
     // ist numerisch?
-    if(!/^-?\d+\.?\d*$/.test(userId)){
-      res.status(400);
-      res.send("userId muss numerisch sein");
+    if (!/^-?\d+\.?\d*$/.test(userId)) {
+      res.status(400).send("userId muss numerisch sein");
       return;
     }
-  }
-  else{
-    res.status(400);
-    res.send("userId benötigt");
+  } else {
+    res.status(400).send("userId benötigt");
     return;
   }
-  if(typeof istGenehmigt === 'undefined'){
+  if (!istGenehmigt) {
     istGenehmigt = 0;
   }
   //-------------------------Überprüfung Parameter---------------------------
 
-  const veranstaltungen = await veranstaltungService.createVeranstaltung(titel, 
-  beschreibung, kontakt, beginn_ts, ende_ts, ortBeschreibung,
-  latitude, longitude, institutionId, userId, istGenehmigt ? 1 : 0); 
-  
-  if(veranstaltungen){
+  const veranstaltungen = await veranstaltungService.createVeranstaltung(
+    titel,
+    beschreibung,
+    kontakt,
+    beginn_ts,
+    ende_ts,
+    ortBeschreibung,
+    latitude,
+    longitude,
+    institutionId,
+    userId,
+    istGenehmigt ? 1 : 0
+  );
+
+  if (veranstaltungen) {
     res.send(veranstaltungen);
-    return
-  }
-  else{
+    return;
+  } else {
     res.sendStatus(400);
-    return
+    return;
   }
-  
 });
 
 module.exports = router;
