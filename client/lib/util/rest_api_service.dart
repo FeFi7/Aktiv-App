c487a7e28ba15cb2d203d7d27a284acd7b017436
Morn98 <nentwig@mail.hs-ulm.de>
2021-04-14 10:27:41 +0200

    Registrierung mit/ohne PLZ hinzugefügt, Bekomme neuen AccessToken mit RefreshToken hinzugefügt, Get User hinzugefügt

--- client/lib/util/rest_api_service.dart
+++ client/lib/util/rest_api_service.dart
@@ -22,11 +22,84 @@ Future<String> attemptLogIn(String mail, String passwort) async {
   return response.body;
 }
 
-/* Future<int> attemptSignUp(String email, String password) async {
-  var res = await http.post(Uri.http(SERVER_IP, '/signup'),
-      body: {"email": email, "password": password});
-  return res.statusCode;
-} */
+Future<String> attemptSignUpWithPLZ(
+    String mail, String passwort, String plz) async {
+  Map<String, dynamic> body = {'mail': mail, 'passwort': passwort, 'plz': plz};
+
+  final response = await http.post(Uri.http(SERVER_IP, '/api/user/signup'),
+      headers: <String, String>{
+        'Content-Type': "application/x-www-form-urlencoded"
+      },
+      body: body,
+      encoding: Encoding.getByName("utf-8"));
+
+  if (response.statusCode == 200) {
+    print("Registrierung erfolgreich");
+  } else {
+    print(response.statusCode);
+  }
+
+  print(response.body);
+
+  return response.body;
+}
+
+Future<String> attemptSignUp(String mail, String passwort) async {
+  Map<String, dynamic> body = {'mail': mail, 'passwort': passwort};
+
+  final response = await http.post(Uri.http(SERVER_IP, '/api/user/signup'),
+      headers: <String, String>{
+        'Content-Type': "application/x-www-form-urlencoded"
+      },
+      body: body,
+      encoding: Encoding.getByName("utf-8"));
+
+  if (response.statusCode == 200) {
+    print("Registrierung erfolgreich");
+  } else {
+    print(response.statusCode);
+  }
+
+  print(response.body);
+
+  return response.body;
+}
+
+Future<String> attemptNewAccessToken(String refreshToken) async {
+  Map<String, dynamic> body = {'token': refreshToken};
+
+  final response = await http.post(Uri.http(SERVER_IP, 'api/user/token'),
+      headers: <String, String>{
+        'Content-Type': "application/x-www-form-urlencoded"
+      },
+      body: body,
+      encoding: Encoding.getByName("utf-8"));
+
+  if (response.statusCode == 200) {
+    print("Neuer AccessToken vorhanden");
+  } else {
+    print(response.statusCode);
+  }
+
+  return response.body;
+}
+
+Future<String> attemptGetUser(String mail) async {
+  Map<String, dynamic> qParams = {'mail': mail};
+
+  final response = await http.get(Uri.http(SERVER_IP, 'api/user/', qParams),
+      headers: <String, String>{
+        'Content-Type': "application/x-www-form-urlencoded"
+      });
+
+  if (response.statusCode == 200) {
+    print("GET User erfolgreich");
+  } else {
+    print(response.statusCode);
+  }
+
+  return response.body;
+}
 
 Future<int> testapi() async {
   final response = await http.get(Uri.http('85.214.166.230', 'api/'));
